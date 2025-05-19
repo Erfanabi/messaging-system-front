82a257daf10c896f50f4b14ba45dc8f4be210d8b
erfan sharafi <erfansharafi60@gmail.com>
2025-05-19 15:56:21 +0330

    feat: integrate HeroUI components and update dependencies
    Add HeroUI library for UI components, update React dependencies, and refactor FormInput component. Include HeroUIProvider in the layout and configure Tailwind with HeroUI theme. Add a new logo image and update the homepage to use HeroUI components for better UI consistency and functionality.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import { Input, Textarea } from "@heroui/input";
+import FormInput from "@/components/FormInput";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container mx-auto w-full max-w-2xl py-20">
+      <div className="relative mb-14 h-1.5 w-full bg-primary">
+        <div className="absolute -top-8 left-1/2 -translate-x-1/2 bg-white px-3">
+          <Image src={"/logo.png"} alt="/logo.png" width={256} height={256} />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-8">
+        <p className="font-semibold text-gray-600">
+          Please enter your details.
+        </p>
+
+        <div className="flex items-start gap-x-8">
+          <FormInput label="Phone Number" />
+
+          <FormInput label="Phone Number" />
+        </div>
+
+        <div className="flex items-start gap-x-8">
+          <FormInput label="WhatsAap" />
+
+          <FormInput label="Hotel Name" />
+        </div>
+
+        <div className="flex items-start gap-x-8">
+          <div className="w-full space-y-1">
+            <label className="block text-sm font-light">
+              Name Phone Number
+            </label>
+            <Textarea
+              color="primary"
+              variant="faded"
+              size="md"
+              radius="sm"
+              className="w-full"
+              classNames={{ inputWrapper: "border-1" }}
+            />
+          </div>
+
+          <FormInput label="Position Address" />
+        </div>
+      </div>
+
+      <div className="relative my-20 mb-14 h-1.5 w-full bg-primary">
+        <div className="absolute -top-8 left-1/2 -translate-x-1/2 bg-white px-3">
+          <Image src={"/logo.png"} alt="/logo.png" width={256} height={256} />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-8">
+        <p className="font-semibold text-gray-600">
+          Check the item you need and write a description.
+        </p>
+
+        <FormInput label="Phone Number" />
+
+        <FormInput label="Phone Number" />
+
+        <FormInput label="Phone Number" />
+
+        <FormInput label="Phone Number" />
+
+        <FormInput label="Phone Number" />
+
+        <FormInput label="Phone Number" />
+      </div>
     </div>
   );
 }
